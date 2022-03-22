165b7795db35cb20fe02bcfd9394628edc862e95
lixiuhua <lxh_qing@163.com>
2022-03-22 23:26:01 +0800

    react event

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Clock from './Components/Clock/Clock';
+// import Clock from './Components/Clock/Clock';
+import { EventEmitter } from 'events';
 
 // 时钟显隐
 /* class App extends React.Component {
@@ -131,7 +132,7 @@ class B extends React.Component {
   }
 } */
 
-// 以上两种跨组件通信的方式十分麻烦，有没有其他较为简单方便的方式来实现跨组件通信呢？
+/* // 以上两种跨组件通信的方式十分麻烦，有没有其他较为简单方便的方式来实现跨组件通信呢？
 // 1.创建一个上下文对象
 const AppContext = React.createContext();
 // 2.从上下文对象中获取容器组件
@@ -153,7 +154,6 @@ class App extends React.Component {
     )
   }
 }
-
 class Father extends React.Component {
   constructor(props) {
     super(props)
@@ -167,7 +167,6 @@ class Father extends React.Component {
     )
   }
 }
-
 class Son extends React.Component {
   constructor(props) {
     super(props)
@@ -188,5 +187,65 @@ class Son extends React.Component {
       </Consumer>
     )
   }
+} */
+
+// events
+// 1.在全局创建一个全局的事件管理器对象
+const eventBus = new EventEmitter();
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+  // 组件挂载到页面上
+  componentDidMount() {
+    eventBus.addListener('hello', this.appFn.bind(this));
+  }
+  // 组件即将卸载
+  componentWillUnmount() {
+    eventBus.removeListener('hello', this.appFn.bind(this));
+  }
+  render() {
+    return (
+      <div>
+        <p>我是爷爷</p>
+        <Father></Father>
+      </div>
+    )
+  }
+  appFn(a, b) {
+    console.log(a, b);
+  }
 }
+class Father extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <p>我是爸爸</p>
+        <Son></Son>
+      </div>
+    )
+  }
+}
+class Son extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <p>我是孙子</p>
+        <button onClick={()=>{this.btnClick()}}>孙子的按钮</button>
+      </div>
+    )
+  }
+  btnClick() {
+    eventBus.emit('hello', 'beijing', '666');
+  }
+}
+
+// props和state区别
+// props用于存储父组件传递进来的数据，为只读的；state存储的是自己的数据，为可读可写的 
 export default App;
